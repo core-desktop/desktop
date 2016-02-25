1aea4bf5c76f72ce443b3b0878f5edef00e5a524
he <he@DawnWu-PC>
2016-02-25 09:56:50 +0800

    添加测试工具

---
+++ src/test/java/Auto.java
@@ -0,0 +1,26 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+/**
+ * 生成控制器类和实体类
+ * @author he
+ *
+ */
+public class Auto  {
+	@Test
+	public void create() throws Exception{
+		Map<String, String> maps = new HashMap<String, String>();
+		maps.put("cls", "model.sys.User");
+		maps.put("desc", "用户");
+		maps.put("cextend", "BaseController");
+		maps.put("appPath", "E:/workspace/desktop/src/main/java/");
+		maps.put("package", "com.htrj.web");
+		
+		com.htrj.core.util.Auto.init();
+		com.htrj.core.util.Auto.generate(maps);
+	}
+
+}
